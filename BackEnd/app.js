cfaa02866a0c58d9787cae94dd730783adb45925
Syed Haris Bukhari <45495476+HarisBukhari@users.noreply.github.com>
2023-01-24 12:51:22 +0500

    Mongo Connection

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -2,15 +2,16 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
-import dotenv from 'dot'
-import morgan from'morgan'
+import dotenv from 'dotenv'
 import helmet from 'helmet'
 import morgan from 'morgan'
-import clientRotes from './routes/client'
-import generalRoutes from './routes/general'
-import managementRoutes from './routes/management'
-import salesRoutes from './routes/sales'
-
+import clientRotes from './routes/client.js'
+import generalRoutes from './routes/general.js'
+import managementRoutes from './routes/management.js'
+import salesRoutes from './routes/sales.js'
+import xss from 'xss-clean'
+import rateLimiter from 'express-rate-limit'
+import connectDB from './db/connect.js'
 
 /* CONFIGURATION */
 dotenv.config()
@@ -22,6 +23,14 @@ app.use(morgan("common"))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
+app.use(xss())
+app.set('trust proxy', 1)
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+)
 
 
 /* Route configuration */
@@ -29,3 +38,20 @@ app.use('/api/v1/client', clientRotes)
 app.use('/api/v1/general', generalRoutes)
 app.use('/api/v1/managemnt', managementRoutes)
 app.use('/api/v1/sales', salesRoutes)
+
+
+/* Mongoose configuration */
+const Port = process.env.PORT || 5000
+const start = async () => {
+    try {
+      await connectDB(process.env.MONGO_URL)
+      app.listen(Port, () =>
+        console.log(`Server is listening on port ${Port}...`)
+      );
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+  
+  start()
---
+++ BackEnd/db/connect.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+mongoose.set('strictQuery', true)
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+}
+
+export default connectDB
